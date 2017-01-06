89db4bcd65f0454ed41fdbf4aa46730d775364dc
rhsiao33 <rhsiao@logitech.com>
2017-01-06 15:12:36 +0800

    Add files via upload
    Add sample code: G910_Sample and LedSetLighting

---
+++ Sample/LedSetLighting.cpp
@@ -0,0 +1,38 @@
+
+// G910_SAMPLE.cpp : Defines the class behaviors for the application.
+//
+
+
+#pragma comment(lib, "LogitechLEDLib.lib")
+#include <stdio.h>
+#include <stdlib.h>
+#include "LogitechLEDLib.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	LogiLedInit();
+
+	int red = 100;
+	int green = 0;
+	int blue = 0;
+
+	int major, minor, build = 0;
+	//show sdk version
+    if(!LogiLedGetSdkVersion(&major, &minor, & build))
+    {
+        cout<<L"Could not retrieve SDK version"<<endl;
+    }
+    else
+    {	
+        printf("SDK VERSION:%d.%d.%d \n",major,minor,build);
+    
+    }
+	//set the lighting of device
+	LogiLedSetLighting(red, green, blue);
+	printf("set color red=%d, green=%d, blue=%d  \n",red,green,blue);
+	getchar();
+	LogiLedShutdown();
+}
