61e16f36c6a81e606d4a8cb402a13b82da50ea2e
rhsiao33 <rhsiao@logitech.com>
2017-01-06 17:30:24 +0800

    Add files via upload

---
+++ Sample/LedFlashPulseLighting.cpp
@@ -0,0 +1,47 @@
+
+// G910_SAMPLE.cpp : Defines the class behaviors for the application.
+//
+
+
+#pragma comment(lib, "LogitechLEDLib.lib")
+#include <stdio.h>
+#include <stdlib.h>
+#include "LogitechLEDLib.h"
+#include <iostream>
+#include "windows.h" 
+using namespace std;
+
+int main()
+{
+
+	LogiLedInit();
+	Sleep(50);
+		
+	int red = 100;
+    int green = 0;
+    int blue = 0;
+    int duration = 4000;
+    int interval = 300;
+	//start to flashing
+    LogiLedFlashLighting(red, green, blue, duration, interval);
+	
+	printf("start flashing red=%d, green=%d, blue=%d, duration=%d, interval=%d \n",red,green,blue,duration,interval);
+	Sleep(duration+1000);
+	
+	LogiLedStopEffects();
+	
+	red=0;
+	green=100;
+	duration = 5000;
+	interval = 300;
+	Sleep(50);
+	//start to pulsing
+	LogiLedPulseLighting(red, green, blue, duration, interval);
+	
+	printf("start Pulsing red=%d, green=%d, blue=%d, duration=%d, interval=%d \n",red,green,blue,duration,interval);
+	Sleep(duration+1000);
+	LogiLedStopEffects();
+	getchar();
+	
+	LogiLedShutdown();
+}
