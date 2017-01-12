1871d7d7c366c789f1bc1324f151cdbc8fee1ea2
rhsiao33 <rhsiao@logitech.com>
2017-01-12 10:58:02 +0800

    Update LedFlashPulseLighting.cpp

--- Sample/LedFlashPulseLighting.cpp
+++ Sample/LedFlashPulseLighting.cpp
@@ -15,32 +15,32 @@ int main()
 {
 
     LogiLedInit();
-	Sleep(500);
+    Sleep(500);
 
-	int red = 100;
+    int red = 100;
     int green = 0;
     int blue = 0;
     int duration = 4000;
     int interval = 300;
-	//start to flashing
+    //start to flashing
     LogiLedFlashLighting(red, green, blue, duration, interval);
 	
-	printf("start flashing red=%d, green=%d, blue=%d, duration=%d, interval=%d \n",red,green,blue,duration,interval);
-	Sleep(duration+1000);
+    printf("start flashing red=%d, green=%d, blue=%d, duration=%d, interval=%d \n",red,green,blue,duration,interval);
+    Sleep(duration+1000);
 	
-	LogiLedStopEffects();
+    LogiLedStopEffects();
 	
-	red=0;
-	green=100;
-	duration = 5000;
-	interval = 300;
-	//start to pulsing
-	LogiLedPulseLighting(red, green, blue, duration, interval);
+    red=0;
+    green=100;
+    duration = 5000;
+    interval = 300;
+    //start to pulsing
+    LogiLedPulseLighting(red, green, blue, duration, interval);
 	
-	printf("start Pulsing red=%d, green=%d, blue=%d, duration=%d, interval=%d \n",red,green,blue,duration,interval);
-	Sleep(duration+1000);
-	LogiLedStopEffects();
-	getchar();
+    printf("start Pulsing red=%d, green=%d, blue=%d, duration=%d, interval=%d \n",red,green,blue,duration,interval);
+    Sleep(duration+1000);
+    LogiLedStopEffects();
+    getchar();
 	
-	LogiLedShutdown();
+    LogiLedShutdown();
 }	
